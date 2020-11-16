8af986d2e535a974b86e880fc127e2c93e7867de
Christian Ramirez Gomez <cramir50@uic.edu>
2020-11-16 16:17:28 -0600

    Login and signup working

--- runthis.js
+++ runthis.js
@@ -1,50 +1,71 @@
-// To run as standalone
 const mysqlx      = require('@mysql/xdevapi');
 
-// Connect to server using a connection URL
-var mySession = mysqlx.getSession( {
- host: 'localhost', port: 33060,
- user: 'root', password: '248778'} )
- .then (session =>{
-    var myDb = session.getSchema('university');
-	// Accessing an existing table
-	var myTable = myDb.getTable('student');
-	var myResult = myTable.select(['id', 'name', 'dept_name']).
-	 where('name like :name').
-	 bind('name', 'L%').execute();
-
-	return myResult
- })
- .then(result => {
-	 	// Print result
-	console.log(result.fetchAll());
- });
-
-
-// as a function called by server
 module.exports =
 {
-buildPromise : async function ()
+twoVar : async function (x, y, q)
 {
   const mysqlx      = require('@mysql/xdevapi');
 
   // Connect to server using a connection URL
   var mySession = mysqlx.getSession( {
    host: 'localhost', port: 33060,
-   user: 'root', password: '248778'} )
+   user: 'root', password: '248778',
+   schema:'anime_project'} )
    .then (session =>{
-     var myDb = session.getSchema('university');
-     // Accessing an existing table
-     var myTable = myDb.getTable('student');
-     var myResult = myTable.select(['id', 'name', 'dept_name']).
-	   where('name like :name').
-	   bind('name', '%').execute();
-
-	return myResult
+     
+	 var myResult = session.sql(q).bind(x, y).execute();
+	 return myResult
  })
  .then(result => {
 	return result.fetchAll();
  });
  return mySession;
 }
+,
+
+
+signupAccount : async function (x, q)
+{
+  const mysqlx      = require('@mysql/xdevapi');
+
+  // Connect to server using a connection URL
+  var mySession = mysqlx.getSession( {
+   host: 'localhost', port: 33060,
+   user: 'root', password: '248778',
+   schema:'anime_project'} )
+   .then (session =>{
+     
+	 var myResult = session.sql(q).bind(x).execute();
+	 return myResult
+ })
+ .then(result => {
+	return result.fetchAll();
+ });
+ return mySession;
 }
+
+
+
+}
+// module.exports =
+// {
+// signupAccount : async function (u, p)
+// {
+//   const mysqlx      = require('@mysql/xdevapi');
+
+//   // Connect to server using a connection URL
+//   var mySession = mysqlx.getSession( {
+//    host: 'localhost', port: 33060,
+//    user: 'root', password: '248778',
+//    schema:'anime_project'} )
+//    .then (session =>{
+     
+// 	 var myResult = session.sql('SELECT signupAccount(?,?)').bind(u, p).execute();
+// 	 return myResult
+//  })
+//  .then(result => {
+// 	return result.fetchAll();
+//  });
+//  return mySession;
+// }
+// }
--- serverExpress.js
+++ serverExpress.js
@@ -2,14 +2,70 @@ var express = require('express')
 var app = express()
 var path = require('path')
 
-var runQuery = require('./runthis.js')
+//query strings
+var loginQuery = 'CALL loginInfo(?,?)';
+var signupQuery = 'CALL signupAccount(?,?)';
 
+//var runQuery = require('./runthis.js')
+const {twoVar, oneVar} = require('./runthis.js');
+var bodyParser = require('body-parser');
+var urlencodedParser = bodyParser.urlencoded({extended: false});
+
+
+
+//Pages
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname + '/index.html'))
+
 })
-app.get('/db', async function (req, res) {
-	var result = await runQuery.buildPromise()
-	res.send(result)
+app.get('/signup.html', function (req, res) {
+  res.sendFile(path.join(__dirname + '/pages/signup.html'))
 })
+app.get('/profile.html', function (req, res) {
+  res.sendFile(path.join(__dirname + '/pages/profile.html'))
+})
+
+//stylesheets
+app.get('/styleLogin.css', function (req, res) {
+  res.sendFile(path.join(__dirname + '/pages/style/styleLogin.css'))
+})
+app.get('/styleProfile.css', function (req, res) {
+  res.sendFile(path.join(__dirname + '/pages/style/styleProfile.css'))
+})
+
+//Images
+app.get('/user.png', function (req, res) {
+  res.sendFile(path.join(__dirname + '/graphics/user.png'))
+})
+
+//Request queries
+//Used to login to account
+app.post('/', urlencodedParser, async function(req, res) { 
+  console.log(req.body);
+  var u = (req.body.u); 
+  var p = (req.body.p); 
+  
+  var result = await twoVar(u, p, loginQuery);
+  console.log(result);
+  res.send(result); 
+}); 
+//used to sign up for a new account
+app.post('/pages/signup.html', urlencodedParser, async function(req, res) { 
+  console.log(req.body);
+  var u = (req.body.u); 
+  var p = (req.body.p); 
+  
+  var result = await twoVar(u, p, signupQuery);
+  console.log(result);
+  res.send(result); 
+}); 
+  
+
+// app.get('/db', async function (req, res) {
+// 	var result = await runQuery.buildPromise('Chris27153', 248778)
+// 	res.send(result) 
+// })
+
+
 
 app.listen(3000)
